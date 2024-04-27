fa2178ef7e3961ebd109a61393fca14044bd98b7
NitinSingh2608 <nitinvks26@gmail.com>
2024-04-27 14:18:16 +0000

    added script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Export the GitHub username
-export GITHUB_USERNAME="<username>"
+export GITHUB_USERNAME="NitinSingh2608"
 
 # Export the GitHub repository name
-export GITHUB_REPOSITORY="se2001-<roll_number>"
+export GITHUB_REPOSITORY="se2001-21f3003210"
